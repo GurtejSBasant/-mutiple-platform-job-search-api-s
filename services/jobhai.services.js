d6d09d0f142bb3ff3535b47982c224f19dfe780a
aditya-seth-303 <gurtej@sourcebae.com>
2024-09-19 15:46:26 +0530

    Authorization token exchange

--- services/jobhai.services.js
+++ services/jobhai.services.js
@@ -39,7 +39,7 @@ class JobhaiService {
       'Accept': 'application/json, text/plain, */*',
       'Accept-Encoding': 'gzip, deflate, br, zstd',
       'Accept-Language': 'en-US,en;q=0.9',
-      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjU5NjYwMzcsImV4cCI6MTc1NzUwMjAzN30.GcbhP3Vi5jrx4Ee3mtOABRWuXRT8bzFdqRTC4phrXNQ', // Replace this with a valid token
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjY3MzExNzEsImV4cCI6MTc1ODI2NzE3MX0.eOV1btlTCtZX_P3u6myuF_WkC7RKVrl9fNwl2dvamy0', // Replace this with a valid token
       'Content-Type': 'application/json;charset=UTF-8',
       'Deviceid': '5d1679ee-3690-466b-9799-ceaf0074a4f4', // Make sure this matches your device ID
       'Language': 'en',
@@ -82,7 +82,7 @@ class JobhaiService {
         'Accept': 'application/json, text/plain, */*',
         'Accept-Encoding': 'gzip, deflate, br, zstd',
         'Accept-Language': 'en-US,en;q=0.9',
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjU5NjYwMzcsImV4cCI6MTc1NzUwMjAzN30.GcbhP3Vi5jrx4Ee3mtOABRWuXRT8bzFdqRTC4phrXNQ', // Replace this with a valid token
+        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjY3MzExNzEsImV4cCI6MTc1ODI2NzE3MX0.eOV1btlTCtZX_P3u6myuF_WkC7RKVrl9fNwl2dvamy0', // Replace this with a valid token
         'Content-Type': 'application/json;charset=UTF-8',
         'Deviceid': '5d1679ee-3690-466b-9799-ceaf0074a4f4', // Make sure this matches your device ID
         'Language': 'en',
@@ -156,7 +156,7 @@ class JobhaiService {
       'Accept': 'application/json, text/plain, */*',
       'Accept-Encoding': 'gzip, deflate, br, zstd',
       'Accept-Language': 'en-US,en;q=0.9',
-      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjU5NjYwMzcsImV4cCI6MTc1NzUwMjAzN30.GcbhP3Vi5jrx4Ee3mtOABRWuXRT8bzFdqRTC4phrXNQ',
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjY3MzExNzEsImV4cCI6MTc1ODI2NzE3MX0.eOV1btlTCtZX_P3u6myuF_WkC7RKVrl9fNwl2dvamy0',
       'Device-Id': '5d1679ee-3690-466b-9799-ceaf0074a4f4',
       'Language': 'en',
       'Origin': 'https://www.jobhai.com',
@@ -188,7 +188,7 @@ class JobhaiService {
 static async callJob(payload) {
   const url = `https://api.jobhai.com/jobs/v3/call`;
   const headers = {
-    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjU5NjYwMzcsImV4cCI6MTc1NzUwMjAzN30.GcbhP3Vi5jrx4Ee3mtOABRWuXRT8bzFdqRTC4phrXNQ',
+    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjY3MzExNzEsImV4cCI6MTc1ODI2NzE3MX0.eOV1btlTCtZX_P3u6myuF_WkC7RKVrl9fNwl2dvamy0',
     'Content-Type': 'application/json;charset=UTF-8',
     'Device-Id': '5d1679ee-3690-466b-9799-ceaf0074a4f4',
     'Language': 'en',
@@ -220,7 +220,7 @@ static async callJob(payload) {
       'Accept': 'application/json, text/plain, */*',
       'Accept-Encoding': 'gzip, deflate, br, zstd',
       'Accept-Language': 'en-US,en;q=0.9',
-      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjU5NjYwMzcsImV4cCI6MTc1NzUwMjAzN30.GcbhP3Vi5jrx4Ee3mtOABRWuXRT8bzFdqRTC4phrXNQ',
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MjY3MzExNzEsImV4cCI6MTc1ODI2NzE3MX0.eOV1btlTCtZX_P3u6myuF_WkC7RKVrl9fNwl2dvamy0',
       'Content-Length': '57',
       'Content-Type': 'application/json;charset=UTF-8',
       'Cookie': '_ga=GA1.1.782455124.1719569461; deviceId=5d1679ee-3690-466b-9799-ceaf0074a4f4; isUstSaved=true; utm_source=google; utm_medium=organic; referrer=https://www.google.com/; deviceIdVerified=true; access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJfaWQiOjExMzM5MjI2LCJzdGF0dXMiOiJ2ZXJpZmllZCJ9LCJpYXQiOjE3MTk1Njk0OTYsImV4cCI6MTc1MTEwNTQ5Nn0.BJS0NbDS3mkctB-nNICn2sb105HNkzC5_hrfEkjB8Aw; access_id=11339226; _fbp=fb.1.1719569501240.262571050242179048; isSessionRunning=true; _abck=3CB5244BAEC6BE324703A1C51F89065B~-1~YAAQznMsMaRVQk6QAQAAcorDbAyklUm76oubKVZcqPX2mu5erDhe6n3fe76C8yeBlHnzWV3wDBF2VJ9+45R4tIJvwrzNwkdHOrpj2Ph60Hf8Q8tC0dSabtYinf+JlXfV3auiPvZrq1TT6Thnn8UgODtQK6cPxSE+TLgd3iGoSbwZLxAO954HsQMAMm4UV2B3pNgVjoGnB0X/pyi48rs51sPF++uh7gdmjJNd+E8VtROpvFuaS3N+LFWD/vvIGcgUY1TMQzregrSM40bAB1i1ZWNXqS/ATzha7Z5PdfyCA/g78PGgDR71zCgaU5Blt+jhKxSfH9KypQ0o2Mj8vIMaUoPzyrdpbXOkL/zipVwl2EUqQpPHHwcnuiRHsma8ArH1yjKjnqK+kfbwkA==~-1~-1~-1; bm_mi=7A9C770E937888B743987CECC5107292~YAAQznMsMbVVQk6QAQAA0orDbBhUorY3EM2+3TsUxS7M9Ff50PIE08OF2F3Ht3b1QJn2aEIlLPiLMEATy3Hic4AEzF42/HOxhgalr6aacoKPNugOGMJP3HwowHZni551GKX1RJHYe0uDAXoNIIHlc4p6tQLcl6hpNuVW5dir36yx+3rUMqrwG1dXcG/uxCMzrGarK0qqJXR1pYZ3f9qwdOdNqxG5O1ix4w+qZR6EOGQAIVGCVwTMr0QpBHTqpBCEaFwopXyvb2rMXrTdu5S+26MELlKGOtmvdoaVf04YTd8RBz1v+SLg6PW2b1lcQBhHJjMx0no=~1; bm_sz=38A1665CA046D56E424852CB4714CDFB~YAAQznMsMbdVQk6QAQAA0orDbBiLRdXnJnU1wddicg5Iig9pSzfJK9KMN11kcjhEy8jk3WQ0UTRkZus43Cc2aCUz+R1twbWag4KBcu1sdaRZusCowYfrCRI/gdZ0L8Fn46Uhorbk1tvFjq/DY8mWd9ToNB2XruDgoimIpir6Xho3sPcgk2R8lLVbc5k/mQOt9RGNKehSgXyP6qKeJgsb3XldR1NeznVwUyohxYeSs42m56QOAd1g6q5uEtHswj0iyWYYDEVfHR5P9/Duugco8T2z0xqRCCu8BiUsFTLXH5LyKO1v+jcb7+ssM8t+1fYQe9m/TIE5whifbGw+nEMZsh7+W4XK7VvT/tYjGuo6G9hS8P5XzdnkktNaBT/9Cm3EI3c2+EPZy0+H1Xe19BrI~3421239~3682881; ak_bmsc=684AFA2AABD2DFB29CBED619A53A567A~000000000000000000000000000000~YAAQznMsMVNWQk6QAQAAjI7DbBjWnDyBeYxMgdf9iTl9ndJxRzoRlixEcCS4iRkzFVdIDQ5JpPTbe0bnrZ7E4/PHInqDHSRWZRyyfZj/adeMRUx0q5Yl46rh63YH0OyTVwsobJ5QUOfFAOsyVScKeGn5f/AG8jXsEwDng9E9ei1UsZ+01wM3eZLgDIGpvWJ/VRELhOc89pBPEkxshIaHbqSZq468kEN/gRApdGZ+yeadjTy/BoHxkuHoqY0Mf5sbmYkbCT82S9KoqFmkESvrK/ogY1j72l++eRSrxMwDrrHSBIG1XSigE0hPsYgY0B7bJNsXri9VYqVJL2MTnrRA24rfkThIv1Q1wLI5CX+uNG9gbTHuzjUsBXsxjoUfsdoN060EEpIwqR0oe4mTHOC16RMwrPLNWTCaz1WyWJ8R30dZNhH9LcB6qgjPvfaEmRQNd5GCTE5RaLafpO5hZW7BxBc+wuZUyU84Q2MVc3nQjLNq7F2p; moe_uuid=e3dc5c7a-72a4-4a31-87f2-4ba38002492b; pageFiltersAfterLogin=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImNhdGVnb3J5Ijp7IjE4Ijp0cnVlfSwiY29ob3J0X2NhdGVnb3J5Ijp7fX0sImlhdCI6MTcxOTgxMTY3NCwiZXhwIjoxNzIyNDAzNjc0fQ.Tc_OsxBwFTCjfpIimM6q_HgGHo0e9U6GTbeKKcfrPzA; sessionCount=2; isFeedbackSubmit=true; _clck=1lc439x%7C2%7Cfn3%7C0%7C1640; last_visited_page=it-hardware-network-engineer-computer-operator-job-in-workdeal-services-private-limited-keshar-bagh-indore-1-to-3-years-1719664895-4182701-jid; _clsk=17gym58%7C1719811714179%7C2%7C1%7Cr.clarity.ms%2Fcollect; bm_sv=146394C6CAE5F6A83328AB565ED215D9~YAAQznMsMaFoQk6QAQAA/hfEbBgLMsDzHcWh2xZuDAzJU+TC60lNqRQ68ycV9Y2q7sdcLARbwnFkHQIXkyYkwouRyvnAISThY3zX7AlNk8p1OEL7VIrsMfGO2kwgkv8J+oSrTsDQ63V2VSvyZNA1qvMgTjTGGnK8VWQ9AEhjwglnkf3yhBFJeZ1y5KYZDBh2M/udv+Lwr4HjTtLVbd7Caxy0QtrMpXRxdqU9yItQqd0hV4l9SKfolWcClc4CVZqY~1; _ga_WRKNVY33RD=GS1.1.1719811678.2.1.1719811721.17.0.0',
